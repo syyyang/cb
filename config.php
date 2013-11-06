4b210514f48676e2943fa62f2a2b47672ae401be
syyyang <lavaa0000@163.com>
2013-11-06 16:01:40 +0800

    测试添项目
    测试添项目

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+    return array(
+      //  'URL_MODEL'=>3, // 如果你的环境不支持PATHINFO 请设置为3
+        'DB_TYPE'=>'mysql',
+        'DB_HOST'=>'127.0.0.1',
+        'DB_NAME'=>'carunion',
+        'DB_USER'=>'root',
+        'DB_PWD'=>'linuxred',
+        'DB_PORT'=>'3306',
+        'DB_PREFIX'=>'carunion_',
+    );
+?>
